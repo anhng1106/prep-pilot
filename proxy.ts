f3d206a05c273b37741c863a80973076d2b53e7c
anhng1106 <ntvanh1106@gmail.com>
2025-12-30 14:38:22 +0200

    updated dependencies

--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,12 @@
+import { NextResponse } from "next/server";
 import withAuth from "next-auth/middleware";
 import { IUser } from "./backend/models/user.model";
 
-export default withAuth(function middleware(req) {
+export default withAuth(function proxy(req) {
   const url = req?.nextUrl?.pathname;
-  const user = req?.nextauth?.token?.user as IUser;
+  const user = req?.nextauth?.token?.user as IUser | undefined;
+
+  return NextResponse.next();
 });
 
 export const config = {
